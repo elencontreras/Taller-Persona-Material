e7639e7f3dcf5ed6fc11c133232004bfa8a5f4a3
Elen <elencontreras6@gmail.com>
2017-10-14 14:39:11 -0500

    App sin modificar

---
+++ app/src/main/java/com/example/android/personasmaterialdiplomado/Datos.java
@@ -0,0 +1,31 @@
+package com.example.android.personasmaterialdiplomado;
+
+import java.util.ArrayList;
+
+/**
+ * Created by android on 07/10/2017.
+ */
+
+public class Datos {
+    private static ArrayList<Persona> personas = new ArrayList();
+
+    public static void guardarPersona(Persona p) {
+        personas.add(p);
+    }
+
+    public static ArrayList<Persona> obtenerPersonas() {
+        return personas;
+    }
+
+    public static boolean eliminarPersona(Persona p){
+        for (int i = 0; i <personas.size() ; i++) {
+            if(p.getCedula().equals(personas.get(i).getCedula())){
+                personas.remove(i);
+                return true;
+            }
+        }
+        return false;
+
+    }
+}
+
